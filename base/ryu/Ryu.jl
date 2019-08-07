bcbd276a9afe214e65fa4f580888890fb4bcda85
Jacob Quinn <quinn.jacobd@gmail.com>
2019-08-07 10:46:09 -0600

    Fix float printing from using fixed to precision (via rounding)

--- base/ryu/Ryu.jl
+++ base/ryu/Ryu.jl
@@ -15,9 +15,9 @@ function writeshortest(x::T) where {T <: Base.IEEEFloat}
     return unsafe_string(pointer(buf), pos-1)
 end
 
-function writefixed(x::T, precision, trim=false) where {T <: Base.IEEEFloat}
+function writefixed(x::T, precision) where {T <: Base.IEEEFloat}
     buf = Vector{UInt8}(undef, precision + shortestdigits(T))
-    pos = writefixed(x, precision, buf, 1, trim)
+    pos = writefixed(x, precision, buf, 1)
     return unsafe_string(pointer(buf), pos-1)
 end
 
@@ -29,15 +29,11 @@ end
 
 function Base.show(io::IO, x::T) where {T <: Base.IEEEFloat}
     if get(io, :compact, false)
-        precision = T == Float16 ? 5 : 6
-        buf = Vector{UInt8}(undef, precision + shortestdigits(T))
-        pos = writefixed(x, precision, buf, 1, true)
-        GC.@preserve buf unsafe_write(io, pointer(buf), pos - 1)
-    else
-        buf = Vector{UInt8}(undef, shortestdigits(T))
-        pos = writeshortest(x, buf, 1)
-        GC.@preserve buf unsafe_write(io, pointer(buf), pos - 1)
+        x = round(x, sigdigits=5)
     end
+    buf = Vector{UInt8}(undef, shortestdigits(T))
+    pos = writeshortest(x, buf, 1)
+    GC.@preserve buf unsafe_write(io, pointer(buf), pos - 1)
     return
 end
 
